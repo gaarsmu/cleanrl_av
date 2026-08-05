7606d48c2c27579e2c571579023090c599f5801f
HamedDi81 <hamedmasoudi95@gmail.com>
2026-08-05 13:35:02 +0300

    debug

--- benchmark/rrdq.sh
+++ benchmark/rrdq.sh
@@ -6,12 +6,12 @@ export OMP_NUM_THREADS=1
 
 uv pip install .
 
-xvfb-run -a uv run python -m cleanrl_utils.benchmark \
+python -m cleanrl_utils.benchmark \
     --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 LunarLander-v2 \
-    --command "uv run python cleanrl/rrdq.py --torch-deterministic --track --beta 1.0 --total-timesteps 200000 --eval_frequency 1000 --exp_name 'hardrrdq'" \
+    --command "python cleanrl/rrdq.py --torch-deterministic --track --beta 1.0 --total-timesteps 200000 --eval_frequency 1000 --exp_name 'hardrrdq'" \
     --num-seeds 3 \
     --workers 9 \
     --slurm-gpus-per-task 1 \
     --slurm-ntasks 1 \
     --slurm-total-cpus 4 \
-    --slurm-template-path benchmark/triton_1gpu.slurm_template
+    --slurm-template-path benchmark/cleanrl_1gpu.slurm_template
